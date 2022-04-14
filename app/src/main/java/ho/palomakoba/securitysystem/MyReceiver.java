0eb24fcb4793718bab0b7861bcd9ea82fba64f87
Silas P Ladislau <splhead@gmail.com>
2022-04-13 20:13:50 -0400

    initial

---
+++ app/src/main/java/ho/palomakoba/securitysystem/MyReceiver.java
@@ -0,0 +1,14 @@
+package ho.palomakoba.securitysystem;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // init the service after a boot completed
+
+    }
+}
